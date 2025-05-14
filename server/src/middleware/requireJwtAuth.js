c1b07901b07798033d843226292202199b8493cf
Somesh Bhardwaj <somesh@havfly.com>
2025-05-14 13:46:52 +0530

    kuch nhi

--- server/src/middleware/requireJwtAuth.js
+++
@@ -1,7 +0,0 @@
-import passport from 'passport';
-
-const requireJwtAuth = passport.authenticate('jwt', { session: false });
-
-//  Check for token here match with database
-
-export default requireJwtAuth;
